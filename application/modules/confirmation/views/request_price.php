e89162587fd595a3c53aa6c0ebe31a5197f62c03
seagate25 <sigidh.budi@gmail.com>
2022-05-29 02:29:44 +0700

    modules confirmation - request price -  styling table

--- application/modules/confirmation/views/request_price.php
+++ application/modules/confirmation/views/request_price.php
@@ -1,9 +1,9 @@
 <div class="card shadow-sm">
-    <div class="card-header">
-        <h3 class="card-title"><?php echo $title; ?></h3>
+    <div class="card-header bg-success">
+        <h3 class="card-title text-white"><?php echo $title; ?></h3>
         <div class="card-toolbar">
-            <button type="button" class="btn btn-sm btn-primary btn-icon me-2 mb-2">
-            <i class="las la-sync fs-1"></i>
+            <button type="button" class="btn btn-sm btn-bg-white btn-icon me-2 mb-2">
+            <i class="las la-sync fs-1 text-success"></i>
             </button>
         </div>
     </div>
