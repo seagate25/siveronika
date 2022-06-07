b82c5c0bc71a4665a07eb1fa4b067c549e3439fe
Perdiyansyah <muhamad.wik@gmail.com>
2022-06-07 23:09:34 +0700

    Add UI Mockup - RFQ - Status PO - Negosiasi - Riwayat

---
+++ schema_data/Table - TB_S_MST_PROVINSI.sql
@@ -0,0 +1,24 @@
+USE [baragud]
+GO
+
+/****** Object:  Table [dbo].[TB_S_MST_PROVINSI]    Script Date: 04/06/2022 18:01:13 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TB_S_MST_PROVINSI](
+	[kode_provinsi] [char](3) NOT NULL,
+	[nama] [char](50) NULL,
+	[deletion] [char](1) NULL,
+	[modified_date] [datetime] NULL,
+	[modified_by] [char](12) NULL,
+ CONSTRAINT [PK_TB_S_MST_PROVINSI] PRIMARY KEY CLUSTERED 
+(
+	[kode_provinsi] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
