2d1db11c27933ab501802f337d320a7998c6c4d9
perdiyansyah <muhamad.wik@gmail.com>
2022-11-11 17:38:53 +0700

    adjust confirmation view

--- application/modules/confirmation/views/confirmation_price.php
+++ application/modules/confirmation/views/confirmation_price.php
@@ -138,7 +138,7 @@
                         <!--Begin::Input Group-->
                         <div class="row mb-6">
                             <!--begin::Label-->
-                            <label class="col-lg-4 col-form-label required fw-bold fs-6">Nama Material</label>
+                            <label class="col-lg-4 col-form-label fw-bold fs-6">Nama Material</label>
                             <!--end::Label-->
                             <!--begin::Col-->
                             <div class="col-lg-8 fv-row fv-plugins-icon-container">
