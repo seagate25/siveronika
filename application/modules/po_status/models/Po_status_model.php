14fbbf6be664f6f622fd023ffb3ec71280448910
seagate25 <sigidh.budi@gmail.com>
2022-12-16 19:50:31 +0700

    List of Outstanding PO

--- application/modules/po_status/models/Po_status_model.php
+++ application/modules/po_status/models/Po_status_model.php
@@ -245,32 +245,6 @@ class Po_status_model extends CI_Model {
 
     public function getListOsPo()
     {
-        $start = $this->input->post('start');
-        $length = $this->input->post('length') != -1 ? $this->input->post('length') : 10;
-        $draw = $this->input->post('draw');
-        $search = $this->input->post('search');
-        $order = $this->input->post('order');
-        $order_column = $order[0]['column'];
-        $order_dir = strtoupper($order[0]['dir']);
-        $field  = array(
-            1 => 'nomor_po',
-            5 => 'tanggal_document',
-            6 => 'tanggal_dibuat',
-            7 => 'jatuh_tempo'
-        );
-
-        $order_column = $field[$order_column];
-
-        $where = " WHERE (kode_vendor = '{$this->vendor}' AND status = 0) ";
-        if (!empty($search['value'])) {
-            $where .= " AND ";
-            $where .= " (nomor_po LIKE '%" . $search['value'] . "%'";
-            $where .= " OR tanggal_document LIKE '%" . $search['value'] . "%'";
-            $where .= " OR tanggal_dibuat LIKE '%" . $search['value'] . "%'";
-            $where .= " OR jatuh_tempo LIKE '%" . $search['value'] . "%')";
-        }
-
-
         $sql    = "SELECT
                         CONCAT(purchasing_document, '-', posnr) AS po_number_item,
                         CONCAT(FORMAT(posting_date, 'dd/MM/yyyy'), '-', FORMAT(deadline_date, 'dd/MM/yyyy')) AS order_date_deadline,
@@ -289,76 +263,15 @@ class Po_status_model extends CI_Model {
                         TB_S_TR_PO_OUTSTANDING
                     WHERE
                         vendor = '{$this->vendor}'";
-        $sql_   = "SELECT  *
-                FROM    ( SELECT ROW_NUMBER() OVER ( ORDER BY nomor_po {$order_dir} ) AS RowNum,
-                            nomor_po, tanggal_document, tanggal_dibuat, jatuh_tempo, modified_date, modified_by
-                        FROM      {$this->table['head']}
-                        {$where} ) AS RowConstrainedResult
-                WHERE   RowNum > {$start}
-                    AND RowNum < (({$start} + 1) + {$length})
-                ORDER BY RowNum";
-
-        $query = $this->db->query($sql_);
-        $rows_data = $query->result();
-        $rows = array();
-        $i = (0 + 1);
-
-        $scan_po_files = directory_map('upload_files/Dokumen_PO');
-
-        foreach ($rows_data as $row) {
-        $row->po_no                 = '';
-        $link_attachment_po         = '';
-        if(in_array($row->nomor_po.'.pdf', $scan_po_files)) {
-            $row->po_no             = '<a href="'. site_url('po_status/download/head/'.$this->crypto->encode($row->nomor_po)) .'">'.$row->nomor_po.'</a>';
-        } else {
-            $row->po_no             = $row->nomor_po;
+        $query  = $this->db->query($sql);
+        $number = (0 + 1);
+        $data   = $query->result();
+        foreach($data as &$row) {
+            $row->number    = $number;
+            $row->material_num_desc = utf8_encode($row->material_num_desc);
         }
-
-        // if(strpos(json_encode($scan_po_files), $row->nomor_po.'_') !== FALSE) {
-        //     $link_attachment_po     = site_url('po_status/download/detail/'.$this->crypto->encode($row->nomor_po));
-        // } else {
-        //     $link_attachment_po     = '#';
-        // }
-
-        $attachmentFiles    = $this->getPOAttachment(['nomor_po' => $row->nomor_po]);
-        if($attachmentFiles->num_rows() > 0) {
-            $row->attachment_po         = '<a href="' . site_url('po_status/download/detail/'.$this->crypto->encode($row->nomor_po)) . '" class="btn btn-icon btn-sm btn-primary me-2 mb-2">
-                                            <i class="fas fa-download text-white"></i>
-                                        </a>';
-        } else {
-            $row->attachment_po         = '<button disabled class="btn btn-icon btn-sm btn-primary me-2 mb-2">
-                                            <i class="fas fa-download text-white"></i>
-                                        </button>';
-        }
-
-        $row->number                = $i;
-        // $row->attachment_po         = '<a href="' . $link_attachment_po . '" class="btn btn-icon btn-sm btn-primary me-2 mb-2">
-        //                                     <i class="fas fa-download text-white"></i>
-        //                                 </a>';
-        $row->template              = '<a href="'. site_url('po_status/download/template/'.$this->crypto->encode($row->nomor_po)) .'" class="btn btn-icon btn-sm btn-primary me-2 mb-2">
-                                            <i class="fas fa-download text-white"></i>
-                                        </a>';
-        $row->upload                = '<a href="#" class="btn btn-icon btn-sm btn-primary me-2 mb-2" data-bs-toggle="modal" data-bs-id="'.$this->crypto->encode($row->nomor_po).'" data-bs-target="#kt_modal_upload_po_goods">
-                                            <i class="fas fa-upload text-white"></i>
-                                        </a>';
-        $row->tanggal_document      = date('d.M.y', strtotime($row->tanggal_document));
-        $row->tanggal_dibuat        = date('d.M.y', strtotime($row->tanggal_dibuat));
-        $row->jatuh_tempo           = ($row->jatuh_tempo === NULL) ? '-' : date('d.M.y', strtotime($row->jatuh_tempo));
-        // $row->actions               = '<a href="' . site_url('negotiation/det_rfq_goods/' . $this->crypto->encode($row->nomor_po)) . '" class="btn btn-icon btn-sm btn-success me-2 mb-2">
-        //                                     <i class="fas fa-envelope-open-text"></i>
-        //                                 </a>';
-        $row->actions               = '';
-
-        $rows[] = $row;
-        $i++;
-        }
-
-        return array(
-        'draw' => $draw,
-        'recordsTotal' => $records_total,
-        'recordsFiltered' => $records_total,
-        'data' => $rows,
-        );
+        
+        return ['data' => $data];
     }
 }
 
--- application/modules/po_status/views/list_os_po.php
+++ application/modules/po_status/views/list_os_po.php
@@ -2,7 +2,7 @@
     <div class="card-header bg-success">
         <h3 class="card-title text-white"><?php echo $title; ?></h3>
         <div class="card-toolbar">
-            <button type="button" class="btn btn-sm btn-bg-white btn-icon me-2 mb-2">
+            <button type="button" class="btn btn-sm btn-bg-white btn-icon me-2 mb-2" onclick="return KTDataTables.init();">
             <i class="las la-sync fs-1"></i>
             </button>
         </div>
@@ -12,10 +12,11 @@
             <thead>
                 <tr class="fw-bold fs-6 text-muted">
                     <th class="min-w-50px text-center">No.</th>
-                    <th class="min-w-80px text-center">PO Number - Item</th>
-                    <th class="min-w-50px text-center">Order Date - Deadline</th>
+                    <th class="min-w-150px text-center">PO Number - Item</th>
+                    <th class="min-w-200px text-center">Order Date - Deadline</th>
+                    <th class="min-w-100px text-center">Vendor Name</th>
                     <th class="min-w-50px text-center">Dept / Estate</th>
-                    <th class="min-w-50px text-center">Material Num - Desc</th>
+                    <th class="min-w-400px text-center">Material Num - Desc</th>
                     <th class="min-w-50px text-center">UoM ng Qty</th>
                     <th class="min-w-50px text-center">Outstanding Value</th>
                     <th class="min-w-50px text-center">Late Days</th>
@@ -39,13 +40,26 @@ var KTDataTables = (function() {
     return {
         init: function() {
             e = $("#kt_datatable_example_1").DataTable({
-                processing:!0, 
-                serverSide:!0,
+                processing:!1, 
+                serverSide:!1,
                 destroy: !0,
                 scrollX: !0,
-                dom: "<'row'<'col-sm-12 col-md-12 col-lg-12'f>>" +
-                "<'row'<'col-sm-12'tr>>" +
-                "<'row'<'col-sm-12 col-md-1'l><'col-sm-12 col-md-3'i><'col-sm-12 col-md-8'p>>",
+                dom: "<'row'<'col-sm-6 col-md-6 col-lg-6 d-flex align-items-center'B ><'col-sm-6 col-md-6 col-lg-6'f>>" +
+                    "<'row'<'col-sm-12'tr>>" +
+                    "<'row'<'col-sm-12 col-md-1'l><'col-sm-12 col-md-3'i><'col-sm-12 col-md-8'p>>",
+                    buttons: [
+                    {
+                        extend: 'excel', 
+                        exportOptions: { 
+                            modifier: { 
+                                page: 'all', 
+                                search: 'none' 
+                            }
+                        },
+                        text: 'Export',
+                        className: 'btn btn-sm btn-success'
+                    }
+                ],
                 paging: !0,
                 ordering: !0,
                 searching: !0,
@@ -54,26 +68,31 @@ var KTDataTables = (function() {
                     url: "<?php echo site_url('po_status/list_os_po');?>"
                 },
                 columns: [
-                    { data: 'number', className: 'text-center', sortable: false, searchable: false, orderable: false, 
+                    { data: '', className: 'text-center', sortable: true, searchable: false, orderable: true, 
                         render: function (data, type, row, meta) {
                             return meta.row + meta.settings._iDisplayStart + 1;
                         }
                     },
-                    { data: 'po_no', className: 'text-center' },
-                    { data: 'attachment_po', className: 'text-center', sortable: false, searchable: false, orderable: false },
-                    { data: 'template', className: 'text-center', sortable: false, searchable: false, orderable: false },
-                    { data: 'upload', className: 'text-center', sortable: false, searchable: false, orderable: false },
-                    { data: 'tanggal_document', className: 'text-center' },
-                    { data: 'tanggal_dibuat', className: 'text-center' },
-                    { data: 'jatuh_tempo', className: 'text-center' }
-                    // ,{ data: 'actions', className: 'text-center', sortable: false, searchable: false, orderable: false }
+                    { data: 'po_number_item', className: 'text-center' },
+                    { data: 'order_date_deadline', className: 'text-center' },
+                    { data: 'vendor_name', className: 'text-center' },
+                    { data: 'dept_estate', className: 'text-center' },
+                    { data: 'material_num_desc', className: 'text-left' },
+                    { data: 'uom_ng_qty', className: 'text-center' },
+                    { data: 'outstanding_value', className: 'text-center' },
+                    { data: 'late_days', className: 'text-center' },
+                    { data: 'progress_supply', className: 'text-center' },
+                    { data: 'uom', className: 'text-center' },
+                    { data: 'unit_price', className: 'text-center' },
+                    { data: 'order_qty', className: 'text-center' },
+                    { data: 'outstanding_qty', className: 'text-center' }
                 ],
                 lengthMenu: [
                         [5, 10, 15, 25, -1],
                         [5, 10, 15, 25, "All"]
                     ],
                 pageLength: 10,
-                order: [1, 'ASC']
+                // order: [1, 'DESC']
             });
         }
     };
